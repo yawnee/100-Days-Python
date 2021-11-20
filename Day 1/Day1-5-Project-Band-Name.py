7cd5f8c9a53792caa0b4de7db7c90662f28ee45e
yawnee <yiannis.cefalas@live.com>
2021-11-20 03:30:25 -0500

    Completed project for band name generator

---
+++ Day 1/Day1-5-Project-Band-Name.py
@@ -0,0 +1,5 @@
+#https://replit.com/@appbrewery/band-name-generator-end
+
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
