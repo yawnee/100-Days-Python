adda1191abe96838262172f60dc38a85692e6192
yawnee <yiannis.cefalas@live.com>
2021-11-20 03:39:02 -0500

    Completed nested lists

---
+++ Day 4/Day-4-3-NestedLists.py
@@ -0,0 +1,10 @@
+#Nested Lists
+#lists within a list
+
+fruits = ["Strawberries", "Nectarines", "Apples"]
+vegetables = ["Spinach", "Kale", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
+print(dirty_dozen[0][1])
+print(dirty_dozen[1][1])
+##################################################
