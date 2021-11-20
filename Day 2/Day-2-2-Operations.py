8654572cfee93a833b471de41ca0b3aa8738e0a0
yawnee <yiannis.cefalas@live.com>
2021-11-20 03:31:54 -0500

    Completed mathmetical operations

---
+++ Day 2/Day-2-2-Operations.py
@@ -0,0 +1,40 @@
+# print(3 + 5)
+# print(7 - 4)
+# print(3 * 2)
+# print(type(6/3)) # division will always be a float
+# print(2 **2) # ** = exponents
+# print (4 // 2) to make it into an int instead of float
+
+# PEMDAS
+# ()
+# **
+# *
+# /
+# +
+# -
+#print(int(3 * 3 + 3 / 3 - 3))
+
+# # BMI = weight / height**2(m**2)
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# height = int(height)
+# weight = int(weight)
+# print((height))
+#
+# BMI_result = ((weight / (height ** 2)))
+# print(round(BMI_result,2))
+#
+
+#you can make short cuts
+# User scores a point
+score = 0
+score = score + 1 # you can do score +=1 instead to shorten it
+print ( 4 /3 ) # you can do // to convert to int instead of float ( / ) is always a float
+
+#f Strings shortcut
+score = 1
+print(f"your score is {score} ")
+
+
+
+
