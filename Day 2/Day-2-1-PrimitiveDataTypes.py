8d27d34f5aff6204e2bd7d3c8c9e47ccb89b522a
yawnee <yiannis.cefalas@live.com>
2021-11-20 03:31:03 -0500

    Completed primitive data types

---
+++ Day 2/Day-2-1-PrimitiveDataTypes.py
@@ -0,0 +1,27 @@
+#Data types
+
+#String anything in a quote is a string
+#Hello is a string
+
+print("Hello"[4])
+
+#interger
+print(123 + 456)
+print(123_456) #easy way to do long longers on the eyes
+
+#Float (decemial)
+#3.141589
+
+#Boolean
+#True or False
+
+#type can be used to check the type of the syntax
+num_char = len(input("What is your name?"))
+print("Your name has " + str(num_char) + " characters.")
+
+two_digit_number = input("Type a two digit number")
+print(type(two_digit_number))
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+
+
