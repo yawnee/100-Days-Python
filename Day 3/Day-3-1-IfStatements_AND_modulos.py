b1d90230228e2d9fc6ddadddcc3668237e49fa24
yawnee <yiannis.cefalas@live.com>
2021-11-20 03:32:49 -0500

    Completed conditional operators

---
+++ Day 3/Day-3-1-IfStatements_AND_modulos.py
@@ -0,0 +1,31 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm?" ))
+#
+# # == is when value 1 is checking is the same value of value 2
+#
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("You cannot ride the rollercoaster!")
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# The below text is examples of modulars
+# 7 % 2
+# 2 + 2 + 2 + 1
+# 7 % 3
+# 3 + 3 + 1
+
+print(number)
+number = number % 2
+print(number)
+
+if number == 0:
+    print("This is a even number")
+else:
+    print("This is an odd number")
+
