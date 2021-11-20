e778837e8b043a561bc98ea54a4f6982ac8af141
yawnee <yiannis.cefalas@live.com>
2021-11-20 03:29:07 -0500

    Completed string manipulation

---
+++ Day 1/Day1-2-Lab.py
@@ -0,0 +1,12 @@
+#Day 1-2-Lab
+print("Hello world!\nHello world!")
+print("Hello" + " " + "Angela") #Hello Angela
+
+#Fix the code below 👇
+print('\n')
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
