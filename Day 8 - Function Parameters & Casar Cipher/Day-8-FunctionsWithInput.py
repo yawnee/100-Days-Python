613621a6d3a73081ba466622db6a781c49848767
yawnee <yiannis.cefalas@live.com>
2021-11-21 00:40:56 -0500

    completed functions with input

---
+++ Day 8 - Function Parameters & Casar Cipher/Day-8-FunctionsWithInput.py
@@ -0,0 +1,24 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+#parameter is the name of the variable
+#argument is the value of the data
+
+def greet():
+    print("This")
+    print("is my")
+    print("Function")
+
+greet()
+
+#function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+#greet_with_name_variable goes into the function
+greet_with_name("Angela")
+
