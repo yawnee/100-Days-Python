da1876bbfb75bc3e414e355b8e555f2ed0296cc6
yawnee <yiannis.cefalas@live.com>
2021-12-10 03:08:58 -0500

    Created the number guessing game where a user must find the answer and if by choosing the wrong number they will lose a life.

---
+++ Day 12 - Scope & Number Guessing/Day-12-120-NumberGuessingProject.py
@@ -0,0 +1,56 @@
+# Introducing the final project: the number guessing game
+
+import random
+#from art import logo
+
+print("Welcome to the number guessing game!")
+print("I'm thinking of a number between 1 and 100")
+difficulty = input("Choose a difficulty. Type ' easy' or 'hard': ")
+difficulty.lower()
+
+while not difficulty == ('easy' or 'hard'):
+    difficulty = input(f"Error, not the correct answer. Please insert answer: ")
+if difficulty == 'easy':
+    lives = 10
+elif difficulty == 'hard':
+    lives = 5
+print("")
+
+randomNumber = random.randint(1, 100)
+is_game_over = False
+betweenNumber = False
+
+
+def compare(answer, randomNumber):
+    if answer < randomNumber:
+        return "Too low. \nGuess again!\n"
+    elif answer > randomNumber:
+        return "Too high \nGuess again!\n"
+    else:
+        return "You lose"
+
+
+print(f"You have {lives} attempts remaining to guess the number!")
+answer = int(input("Make a guess: "))
+print("")
+while not betweenNumber:
+    if answer > 100 or answer < 0:
+        answer = int(input("Please insert a number between 1 to 100: "))
+    else:
+        betweenNumber = True
+
+print(f"Debugging help!!: Answer: {answer}, RandomNumber: {randomNumber}")
+
+while not is_game_over:
+    print(compare(answer, randomNumber))
+    answer = int(input("Make a guess: "))
+    if answer == randomNumber:
+        print(f"You won! You had {lives} lives remaining!")
+        is_game_over = True
+    else:
+        lives -= 1
+        if lives == 0:
+            print("You ran out of lives!")
+            is_game_over = True
+        elif lives != 0:
+            print(f"You have {lives} lives remaining!")
