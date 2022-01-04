3aad25e5c69fbb4a0089c725e9f75c51eae418ca
yawnee <yiannis.cefalas@live.com>
2022-01-04 01:22:39 -0500

    Completed working with variables, fixing code, inputs, etc..

--- Day 01 - Working with Variables/Day-1-001-Lab.py
+++ Day 01 - Working with Variables/Day-1-001-Lab.py
@@ -3,4 +3,42 @@ print("Day 01 - Working with Variables - Python Print Function")
 print("The function is declared like this:")
 print('print("what to print")')
 
+#Day 01 - Working with Variables-2-Lab
+print("Hello world!\nHello world!")
+print("Hello" + " " + "Angela") #Hello Angela
+
+#Fix the code below 👇
+print('\n')
+print("Day 01 - Working with Variables - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#input is the input function to prompt to the user to expect from the user to answer
+
+#print("Hello " + input("What is your name?"))
+
+print(len(input("What is your name?")))
+
+#variable renaming to reverise the score
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
 
