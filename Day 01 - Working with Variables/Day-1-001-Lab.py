b277b95176f31c4b1eed1c02be8406e49085df95
yawnee <yiannis.cefalas@live.com>
2022-01-04 01:18:53 -0500

    Fixed formatting

---
+++ Day 01 - Working with Variables/Day-1-001-Lab.py
@@ -0,0 +1,6 @@
+#Day 01 - Working with Variables-1-Lab
+print("Day 01 - Working with Variables - Python Print Function")
+print("The function is declared like this:")
+print('print("what to print")')
+
+
